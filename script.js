e3955350bb7aca1977e358d4cfdc8592dffc4342
annietiet <annietiet@yahoo.com>
2017-07-25 15:23:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-function
+//document.getElementById("dougroute").innerHTML = "this is a test";
+
+function myFunction() {
+var playername = document.getElementById("name.html/playername").value;
+sessionStorage.setItem("P1", playername)
+var dogname = document.getElementById("dogname").value;
+sessionStorage.setItem("CPU", dogname)
+
+
+//document.getElementById("dougroute").innerHTML = "Hey, " + localStorage.getItem("P1") + "! It's me, "+dogname+"!";
+//document.getElementById("dougroute").innerHTML = "this is a test";
+}
